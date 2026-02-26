52495d181be8a9ccb8a8418f531475b1c060b151
Tuna Mbangula <tunambangula8@gmail.com>
2026-02-26 23:36:23 +0200

    Scheme

--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt, { type SignOptions } from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import { z } from "zod";
